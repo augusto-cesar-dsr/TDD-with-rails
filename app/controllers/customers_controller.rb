6fc2d50f60ec3c2997913dba106eaa9149d636e6
augusto cesar <augusto_cesar.cc@hotmail.com>
2021-11-25 11:16:26 -0300

    added new test in views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,30 @@
 class CustomersController < ApplicationController
   
   def index
-    
+    @customers = Customer.all
   end
 
   def new
     @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to customers_path, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @customer = Customer.find_by_id(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email, :phone, :avatar, :smoker)
   end
 end
--- spec/features/costumer_spec.rb
+++ spec/features/costumer_spec.rb
@@ -34,4 +34,46 @@ feature "customers", type: :feature do
     expect(page).to have_content('Cliente cadastrado com sucesso!')
     expect(Customer.last.name).to eq(customer_name)
   end
+
+  scenario 'Não cadastra um cliente inválido' do
+    visit(new_customer_path)
+    click_on('Criar Cliente')
+    expect(page).to have_content('Name can\'t be blank')
+  end
+
+  scenario 'Mostra um client' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png",
+      smoker: ['S', 'N'].sample
+    )
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+    expect(page).to have_content(customer.smoker)
+  end
+
+  scenario 'Testando a index' do
+    customer1 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png",
+      smoker: ['S', 'N'].sample
+    )
+    customer2 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png",
+      smoker: ['S', 'N'].sample
+    )
+
+    visit(customers_path)
+    expect(page).to have_content(customer1.name).and have_content(customer2.name)
+  end
+
 end
